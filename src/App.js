2a71431dc252b9504b616b104c95ea0d2c433c8d
Matt Kim (김명준) <matt85kim53@gmail.com>
2021-05-25 21:07:02 +0900

    inline and global css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style= {{ padding : '2rem'}}>
+      <h1 className="title">styled components</h1>
+      <button className="btn">click me</button>
     </div>
+   
   );
+
+
+
+
+
 }
 
 export default App;
