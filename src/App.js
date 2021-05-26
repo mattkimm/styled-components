bc8b22a8b32ccc252c4b74d37e192aaa365ad347
mattkimm <matt@nextculture.kr>
2021-05-26 12:16:06 +0900

    extend styled component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import styled from 'styled-components';
-import { BasicTitle } from './components/BasicTitle';
-import { DefaultButton } from './components/Button';
-
-
-
-
+import styled from "styled-components";
+import { BasicTitle } from "./components/BasicTitle";
+import { DefaultButton, HipsterButton } from "./components/Button";
 
 function App() {
   return (
-    <div style= {{ padding : '2rem'}}>
+    <div style={{ padding: "2rem" }}>
       <BasicTitle special>styled components</BasicTitle>
       <BasicTitle>styled components</BasicTitle>
       <DefaultButton>click me</DefaultButton>
-     
+      <HipsterButton>click me</HipsterButton>
     </div>
-   
   );
-
-
-
-
-
 }
 
 export default App;
