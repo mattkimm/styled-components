2d3ba4bd14ba6d330d71322537c453a1e48fe4e6
mattkimm <matt@nextculture.kr>
2021-05-26 12:20:43 +0900

    extend react components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import styled from "styled-components";
-import { BasicTitle } from "./components/BasicTitle";
-import { DefaultButton, HipsterButton } from "./components/Button";
+import { Button } from "@material-ui/core";
+
+const StyledBtn = styled(Button)`
+  text-transform: capitalize;
+`;
 
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
-      <BasicTitle special>styled components</BasicTitle>
-      <BasicTitle>styled components</BasicTitle>
-      <DefaultButton>click me</DefaultButton>
-      <HipsterButton>click me</HipsterButton>
+      <Button color="primary" variant="contained">
+        Hello World
+      </Button>
+      <StyledBtn color="primary" variant="contained">
+        Hello World
+      </StyledBtn>
     </div>
   );
 }
